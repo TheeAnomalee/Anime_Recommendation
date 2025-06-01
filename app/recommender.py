0959225525164c38eb04a7032b97514b04544d88
Thee Anomalee <yasmanicastaneda@proton.me>
2025-06-01 11:18:44 -0700

    Recommender.py is complete with some slight bugs

--- app/recommender.py
+++ app/recommender.py
@@ -11,10 +11,10 @@ anime_df = pd.read_csv(csv_path)
 # print(anime_df.head())
 
 #this removes any anime that has a missing synopsis or name
-anime_df = anime_df[anime_df['synopsis'].notna() & anime_df['name'].notna()]
+anime_df = anime_df[anime_df['synopsis'].notna() & anime_df['title'].notna()].reset_index(drop=True)
 
 #since removing the anime might cause the .csv to skip indexes, here we drop the old index and reset the new one so it forgets about the previous indexes we deleted
-anime_df = anime_df.reset_index(drop=True)
+# anime_df = anime_df.reset_index(drop=True)
 
 #This creates an instance of TfidfVectorizer. stop_words english means it will automatically ignore commond english words like "the" and "and" because they dont add much meaning
 tfidf = TfidfVectorizer(stop_words='english')
@@ -30,5 +30,22 @@ tfidf_matrix = tfidf.fit_transform(anime_df['synopsis'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 #This creates a pandas series. The values are the original row indices aka anime_df.index and the index are the anime names from anime_df. Any duplicates get removed.
-indices = pd.Series(anime_df.index, index=anime_df['name']).drop_duplicates()
+# indices = pd.Series(anime_df.index, index=anime_df['title']).drop_duplicates()
+indices = pd.Series(anime_df.index, index=anime_df['title'])
+indices = indices[~indices.index.duplicated(keep='first')]
 
+
+def recommend_anime(title, num_recommondations=5):
+    if title not in indices:
+        return ["Anime not found."]
+
+    table = indices[title]
+    all_scores = list(enumerate(cosine_sim[table].flatten()))
+    sorted_scores = sorted(all_scores, key=lambda x: x[1], reverse=True)
+    top_scores = sorted_scores[1:num_recommondations+1]
+    
+    anime_indices = [i[0] for i in top_scores]
+
+    return anime_df['title'].iloc[anime_indices].tolist()
+
+print(recommend_anime("Naruto"))
